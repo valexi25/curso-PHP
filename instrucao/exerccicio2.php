3a3c04dd15121fd4efe0e646d2515344cbeaaa2a
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-25 04:50:27 -0300

    experimentando com php
    experiências com php

---
+++ instrucao/exerccicio2.php
@@ -0,0 +1,10 @@
+<?php
+    //esta variable conten dados dapessoa para realizar o exercicio
+    $olho_cor =  "verde claro";
+    //a altura da pessoa 
+    $altura = 1.75;
+
+    echo  $olho_cor;
+    echo $altura;
+
+?>
