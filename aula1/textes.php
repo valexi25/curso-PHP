11142cb06d1077f94a3742989ce115eb7cd20f81
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-25 04:30:21 -0300

    aprendiendo PHP
    fazendo curso em vídeo de PHP

---
+++ aula1/textes.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "novo <br>";
+echo "testo 2 <br>";
+echo "ale <br>";
+
+
+$nome = "villarrole";
+
+echo $nome;
+
+
+
+
+
+?>
---
+++ instrucao/instrucao.php
@@ -0,0 +1,8 @@
+<?php
+echo ("primeira linha <br>");
+echo ("Segunda linha <br>");
+
+if(5 > 2 ){
+    echo ("dentro do if");
+}
+
