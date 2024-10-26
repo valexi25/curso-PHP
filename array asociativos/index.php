018db14e711ea8b4bd3d1c0396f7fc90390047fc
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-26 13:30:25 -0300

    arrays acosiativos
    praticas com arrays asociativos

---
+++ array asociativos/index.php
@@ -0,0 +1,9 @@
+<?php
+    $arr = ['nome'=> 'mordis', 'patas' => 4, 'cor' => 'caramelho'];
+
+    echo $arr['nome'];
+    echo "<br>";
+    echo $arr['patas'];
+    echo "<br>";
+    echo $arr['cor'];
+?>
