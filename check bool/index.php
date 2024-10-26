0405a1c3f97c4d4e74174d5087d2398c67767217
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-26 12:56:38 -0300

    teste do curso
    praticando com boolean

--- check bool/index.php
+++ check bool/index.php
@@ -2,6 +2,15 @@
     $a = true;
 
     if(is_bool($a)){
-        echo "es um boolean";
+        echo "es um boolean <br>";
     }
+
+    if(is_bool(0)){
+        echo "nao es um boolean";
+    }
+    if(is_bool(FALSE)){
+        echo "es um boolean False";
+    }
+    
+
 ?>
