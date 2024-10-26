cfa9a6c2e2ec8edf77cbfc8441845d457f377b5d
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-26 13:22:16 -0300

    trabalhando com arrays
    em este exemplo trabalhamos com arrays em no curso de PHP

---
+++ arrays/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $a = [1,2,3];
+    print_r($a);
+    echo "<br>";
+    echo $a[0];
+    echo "<br>";
+    echo $a[1];
+    echo "<br> distintos tipos de dado dentro do array <br>";
+    $arr =  [27,"matheus",true];
+    print_r($arr);
+    echo "<br>";
+    echo $arr[0];
+
+?>
