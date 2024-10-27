ff6963286cc528c4c91b4ed748c4bb3d0719a13f
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-27 12:42:44 -0300

    precticando com PHP
    exercicio de pretica

--- cessao 2/exercicio 1 variables/index.php
+++ cessao 2/exercicio 1 variables/index.php
@@ -1,4 +1,12 @@
 <?php
+    $nome = 'mathebwmus';
+    $velocidade = 276;
+    $ites =[ "Teto sola" => "não", "motor" => "motor 2.0","blindagem" => FALSE ];
 
+    echo $nome;
+    echo "<br>";
+    echo $velocidade;
+    echo "<br>";
+    print_r($ites);
 
 ?>
