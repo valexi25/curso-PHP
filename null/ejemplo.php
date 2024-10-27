a3d7250db56343f6169ce9ffc551ae4b4dd5d68b
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-27 11:16:45 -0300

    exemplo de null
    trabalhando com Null no curso de video

---
+++ null/ejemplo.php
@@ -0,0 +1,14 @@
+<?php
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)){
+        echo "insera um nome valido";
+    }
+    $nome = "pedro";
+    if(is_null($nome)){
+        echo "insera um nome valido";
+    }
+
+?>
