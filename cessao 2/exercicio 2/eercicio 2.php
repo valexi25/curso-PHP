ca4d894cb3820052306e2403e6d1bea0e9ea80c7
valexi25 <106202030+valexi25@users.noreply.github.com>
2024-10-29 20:35:31 -0300

    exercicios em criação de variables
    experimentando com variável

---
+++ cessao 2/exercicio 2/eercicio 2.php
@@ -0,0 +1,11 @@
+<?php
+    //fazer dois variables com numeros mas a soma deles
+    $numero1 = 3;
+    echo $numero1;
+    echo "<br>";
+    $sumero2 = 5;
+    echo $sumero2;
+    echo "<br>";
+    $soma = $numero1 + $sumero2;
+    echo "A soma dos numero é $soma";
+?>
