a45c562a21a01e27fbe67d999b5c8f39c478b3a5
alexi Villlarroel <villarroelalexi63@gmail.com>
2025-01-02 09:58:17 -0400

    terminou o arquivo do curso do include_function

--- Assets/include/include_function.php
+++ Assets/include/include_function.php
@@ -1,6 +1,13 @@
 <div class='titulo'>Include Função</div>
 <?php
-echo 'Carregando: include_funcao<br>';
-function carregarArquivo(){
-    
-}
+    echo 'Carregando: include_funcao<br>';
+    function carregarArquivo(){
+        include('include_arquivo.php');
+
+        echo $variavel.'<br>';
+        echo soma(2,5).'!<br>';
+    }
+    echo 'Novamente no arquivo Include_funcao <br>';
+    //echo soma(1,8).'!<br>';
+    carregarArquivo();
+    echo '<br>'.soma(3,8);
